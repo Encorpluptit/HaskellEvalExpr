a3c20cc806d6cd823109b6a213fcc20d6ba1f678
BERNARD Damien <damien.bernard@epitech.eu>
2020-10-04 13:57:21 +0200

    add(basicTree): Cleaning Basic tree.

--- src/Main.hs
+++ src/Main.hs
@@ -19,11 +19,8 @@ main :: IO()
 main = do
   args <- getArgs
   case args of
-    [] ->  do
-        exit_error "No args given"
+    [] ->  exit_error "No args given"
     (x:_) -> case exec x of
-         Left msg -> do
-           exit_error $ "Error: " ++ msg
-         Right (v, _) -> do
-           print v
+         Left msg -> exit_error $ "Error: " ++ msg
+         Right (v, _) -> print v
 
