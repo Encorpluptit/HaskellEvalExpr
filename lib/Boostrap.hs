bd7fde92b941b08a4e9249d0dd7c52e67bf8ef55
BERNARD Damien <damien.bernard@epitech.eu>
2020-10-06 20:20:06 +0200

    add(boostrap): Adding Boostrap reloaded.

--- lib/Boostrap.hs
+++ lib/Boostrap.hs
@@ -3,167 +3,24 @@ module Boostrap where
 import Data.Maybe
 import Data.Either
 
--- STEP 2
 
--- EX 1.2.1
----------------------------------------------------------------------------------------------------
---type Error = String
---type Parser a = String -> (Maybe (a , String), Error)
---
---parseChar :: Char -> Parser Char
---parseChar c (x:xs)
---    | x == c = (Just (c, xs), "")
---    | otherwise = (Nothing, "No match found")
---parseChar _ [] = (Nothing, "empty haystack input")
-
--- EX 1.2.2
-
---parseAnyChar :: String -> Parser Char
---parseAnyChar (a:as) x@(b:bs)
---    | a == b = Just (b, bs, "")
---    | otherwise = parseAnyChar as x
---parseAnyChar [] _ = Nothing
---parseAnyChar _ [] = Nothing
-
--- Just remark why we have done parseChar
-
---parseAnyChar :: String -> Parser Char
---parseAnyChar (a:as) x
---    | res /= Nothing = parsed
---    | otherwise = parseAnyChar as x
---        where
---            parsed@(res, _) = parseChar a x
---parseAnyChar [] _ = (Nothing, "empty needle input")
-
-
----------------------------------------------------------------------------------------------------
--- STEP 3
--- EX 1.3.1
-
-
---type Error = String
---data Result a = Result {
---    res::Maybe(a , String),
---    err::Error
---}
---type Parser a = String -> Result a
---
----- TODO: Adding function to pretty print (J'arrive pas à faire une instance deShow pour un type, un newtype, ou data avec un type générique)
---instance Show Result a where
---    show (Result a (str, parser)) = show "coucou"
-
---parseChar :: Char -> Parser Char
---parseChar c (x:xs)
---    | x == c = Result(Just (c, xs), "")
---    | otherwise = Result(Nothing, "No match found")
---parseChar _ [] = Result(Nothing, "empty haystack input")
---
---parseAnyChar :: String -> Parser Char
---parseAnyChar (a:as) x
---    | res /= Nothing = Result(parsed)
---    | otherwise = parseAnyChar as x
---        where
---            Result(parsed@(res, _)) = parseChar a x
---parseAnyChar [] _ = Result(Nothing, "empty needle input")
-
---printParser :: MP -> String
---printParser mp = "modulus " ++ show (modulus mp) ++ ", coeffs: " ++ printCoeffs (modulus mp) (coeffs mp)
-
--------------------
-
--- TODO: Adding utility function to send Error
---failedParsing :: String -> Parser a
---failedParsing err = (Nothing, err)
-
---parseOr :: Parser a -> Parser a -> Parser a
---parseOr one two str
----- TODO: Why "res_a /= Nothing" doesn't work ??
---    | Data.Maybe.isJust res_a = a
---    | Data.Maybe.isJust res_b = b
---    | otherwise = (Nothing, "Parsing \"Or\" failed")
---        where
---            a@(res_a, _) = one str
---            b@(res_b, _) = two str
---
----- TODO: WTF This is not it ???
---parseAnyChar :: String -> Parser Char
---parseAnyChar (a:b:as) x
---    | Data.Maybe.isJust res = parsed
---    | otherwise = parseAnyChar as x
---        where
---            parsed@(res, _) = parseOr (parseChar a) (parseChar b) x
---parseAnyChar (a:as) x
---    | Data.Maybe.isJust res = parsed
---    | otherwise = parseAnyChar as x
---        where
---            parsed@(res, _) = parseChar a x
---parseAnyChar [] _ = (Nothing, "empty needle input")
-
-
----------------------------------------------------------------------------------------------------
--- STEP 3
--- EX 1.3.2
-
-type Error = String
-type Result a = Maybe (a , String)
-type Parser a = String -> Either String (Result a)
-
-parseChar :: Char -> Parser Char
-parseChar c (x:xs)
-    | x == c = Right $ Just (c, xs)
-    | otherwise = Left "No match found"
-parseChar _ [] = Left "No match found"
-
-parseAnyChar :: String -> Parser Char
-parseAnyChar needle@(a:b:as) x
-    | Data.Either.isRight parsed = parsed
-    | otherwise = parseAnyChar as x
-        where
-            parsed = parseOr (parseChar a) (parseChar b) x
-parseAnyChar (a:[]) x = parseChar a x
-parseAnyChar [] _ = Left "No match found"
-
-parseOr :: Parser a -> Parser a -> Parser a
-parseOr one two str
-    | Data.Either.isRight a = a
-    | Data.Either.isRight b = b
-    | otherwise = Left "Parsing 'Or' failed"
-        where
-            a = one str
-            b = two str
-
-parseAnd :: Parser a -> Parser b -> Parser (a,b)
-parseAnd first second str = a
-    where
-        a = case first str of
-            Right _ -> b
-            Left msg -> Left msg
-        b = case second $ tail str of
-            Right _ -> Left "ParseAnd Failed 2l"
---            Right _ -> Right ((fst $ Data.Either.fromRight a, fst $ Data.Either.fromRight b), snd $ Data.Either.fromRight b)
-            Left msg -> Left "ParseAnd Failed 2l"
-
---parseAnd :: Parser a -> Parser b -> Parser (a,b)
---parseAnd first second str
---    | Data.Either.isRight a = c
---    | otherwise = Left "ParseAnd Failed at a"
---        where
---            a = first str
---            b = second $ tail str
---            c = case Data.Either.isRight b of
---                True -> Right ((fst $ Data.Either.fromRight a, fst $ Data.Either.fromRight b), snd $ Data.Either.fromRight b)
---                False -> Left "ParseAnd Failed at b"
-
-----------------
-
+-----------------------------------------------------------------------------------------------------
+---- STEP 1
+---- EX 1.1.1
 --type Parser a = String -> Maybe (a , String)
 --
+-----------------------------------------------------------------------------------------------------
+---- STEP 2
+---- EX 1.2.1
 --parseChar :: Char -> Parser Char
 --parseChar c (x:xs)
 --    | x == c = Just (c, xs)
 --    | otherwise = Nothing
 --parseChar _ [] = Nothing
 --
+-----------------------------------------------------------------------------------------------------
+---- STEP 2
+---- EX 1.2.2
 --parseAnyChar :: String -> Parser Char
 --parseAnyChar needle@(a:b:as) x
 --    | Data.Maybe.isJust parsed = parsed
@@ -173,6 +30,9 @@ parseAnd first second str = a
 --parseAnyChar (a:[]) x = parseChar a x
 --parseAnyChar [] _ = Nothing
 --
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.1
 --parseOr :: Parser a -> Parser a -> Parser a
 --parseOr one two str
 --    | Data.Maybe.isJust a = a
@@ -182,17 +42,219 @@ parseAnd first second str = a
 --            a = one str
 --            b = two str
 --
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.2
 --parseAnd :: Parser a -> Parser b -> Parser (a,b)
---parseAnd first second str
---    | Data.Maybe.isJust a = c
+--parseAnd first second str@(_:xs) =
+--    case first str of
+--         Just r -> case second xs of
+--            Just rs -> Just ((fst r, fst rs), snd rs)
+--            _ -> Nothing
+--         _ -> Nothing
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.3
+--parseAndWith :: ( a -> b -> c ) -> Parser a -> Parser b -> Parser c
+--parseAndWith fct first second s =
+--    case first s of
+--        Just (r, rs) -> case second rs of
+--            Just (r', rs') -> Just (fct r r', rs')
+--            _  -> Nothing
+--        _ -> Nothing
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.4
+--parseMany :: Parser a -> Parser [a]
+--parseMany parser str
+--    | isJust res = res
+--    | otherwise = Just ([], str)
+--        where
+--            res = parseAndWith (\x y -> (x:y)) parser (parseMany parser) str
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.5
+--parseSome :: Parser a -> Parser [a]
+--parseSome parser str
+--    | isJust res = Just ((fst a:snd a), as)
 --    | otherwise = Nothing
 --        where
---            a = first str
---            b = second $ tail str
---            c = case Data.Maybe.isJust b of
---                True -> Just ((fst $ fromJust a, fst $ fromJust b), snd $ fromJust b)
---                False -> Nothing
+--            res = parseAnd parser (parseMany parser) str
+--            Just (a, as) = res
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 4
+---- EX 1.4.1
+--parseUInt :: Parser Int
+--parseUInt str
+--    | isJust res = Just (read x :: Int, xs)
+--    | otherwise = Nothing
+--        where
+--            res = parseSome (parseAnyChar ['0'..'9']) str
+--            Just (x, xs) = res
+--
+--parseInt :: Parser Int
+--parseInt str@(a:as)
+--    | isJust neg = Just (negate x, xs)
+--    | otherwise = parseUInt str
+--        where
+--            neg = parseChar '-' str
+--            Just (x, xs) = parseUInt as
+--
 
+
+-----------------------------------------------------------------------------------------------------
+---- STEP 1
+---- EX 1.1.1
+--type Error = String
+--type Result a = (a , String)
+--type Parser a = String -> Either Error (Result a)
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 2
+---- EX 1.2.1
+--parseChar :: Char -> Parser Char
+--parseChar c (x:xs)
+--    | x == c    = Right (c, xs)
+--    | otherwise = Left "No match found"
+--parseChar _ [] = Left "No match found"
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 2
+---- EX 1.2.2
+--parseAnyChar :: String -> Parser Char
+--parseAnyChar (a:b:as) x =
+--    case parseOr (parseChar a) (parseChar b) x of
+--        Left _  -> parseAnyChar as x
+--        r       -> r
+--parseAnyChar [a] x  = parseChar a x
+--parseAnyChar [] _   = Left "No match found"
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.1
+--parseOr :: Parser a -> Parser a -> Parser a
+--parseOr one two str =
+--    case one str of
+--        Left _  -> case two str of
+--            Left _  -> Left "Parsing 'Or' failed"
+--            r'      -> r'
+--        r -> r
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.2
+--parseAnd :: Parser a -> Parser b -> Parser (a,b)
+--parseAnd first second str@(_:xs) =
+--    case first str of
+--         Right r -> case second xs of
+--            Right rs -> Right ((fst r, fst rs), snd rs)
+--            Left msg -> Left msg
+--         Left msg -> Left msg
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.3
 --parseAndWith :: ( a -> b -> c ) -> Parser a -> Parser b -> Parser c
---parseAndWith
+--parseAndWith fct first second s =
+--    case first s of
+--        Right (r, rs) -> case second rs of
+--            Right (r', rs') -> Right (fct r r', rs')
+--            Left msg -> Left msg
+--        Left msg -> Left msg
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.4
+---- Pattern Guard instead of fct in guard
+--parseMany :: Parser a -> Parser [a]
+--parseMany parser str =
+--    case parseAndWith (:) parser (parseMany parser) str of
+--        Left _  -> Right ([], str)
+--        r       -> r
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 3
+---- EX 1.3.5
+--parseSome :: Parser a -> Parser [a]
+--parseSome parser str =
+--    case parseAnd parser (parseMany parser) str of
+--        Right (a, as)   -> Right (uncurry (:) a, as)
+--        _               -> Left $ "ParseSome failed" ++ str
+--
+-----------------------------------------------------------------------------------------------------
+---- STEP 4
+---- EX 1.4.1
+--parseUInt :: Parser Int
+--parseUInt str =
+--    case parseSome (parseAnyChar ['0'..'9']) str of
+--        Right (x, xs) -> Right (read x :: Int, xs)
+--        _ -> Left "Target is not a UInt"
+--
+--parseInt :: Parser Int
+--parseInt str@(a:as) =
+--    case parseChar '-' str of
+--        Right (x, xs)   -> parseUInt as
+--        _               -> parseUInt str
+--
+--parseTuple :: Parser a -> Parser (a, a)
+--parseTuple p s =
+--    case begin p s of
+--        Right (a, s') -> case middle p s' of
+--            Right (b, s'') -> Right ((a, b), s'')
+--            Left _ -> Left $ "Failed at second number: " ++ s'
+--        Left _ -> Left $ "Failed at first number: " ++ s
+--    where
+--        begin       = ignore (parseChar '(')
+--        middle p    = ignore (parseChar ',') (end p)
+--        end p       = parseAndWith const p (parseChar ')')
+--        ignore p1 p2 = parseAndWith seq p1 p2
 
+
+
+
+---------------------------------------------------------------------------------------------------
+------ PART 2 -------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
+---- STEP 1
+type Error = String
+type Result a = (a , String)
+
+data Parser a = Parser {
+    runParser :: String -> Either Error (Result a)
+}
+
+-----------------------------------------------------------------------------------------------------
+---- STEP 1
+---- EX 2.2.1
+parseChar :: Char -> Parser Char
+parseChar c = Parser fct
+    where
+        fct (x:xs)
+            | x == c    = Right (c, xs)
+            | otherwise = Left "No match found"
+        fct [] = Left "No match found"
+
+--parseAnyChar :: String -> Parser Char
+--parseAnyChar (a:b:as) x =
+--    case parseOr (parseChar a) (parseChar b) x of
+--        Left _  -> parseAnyChar as x
+--        r       -> r
+--parseAnyChar [a] x  = parseChar a x
+--parseAnyChar [] _   = Left "No match found"
+
+parseOr :: Parser a -> Parser a -> Parser a
+parseOr one two =
+    case one of
+        Parser {runParser= s -> Left _} -> two
+--        Parser {} -> case two of
+--            Left _  -> Parser {runParser=Left "Parsing 'Or' failed" }
+--            r'      -> Parser r'
+        r -> r
+--    where
+--        f = Parser
+--parseOr :: Parser a -> Parser a -> Parser a
+--parseOr one two = one two
