f9b4c5e8bb0572806799fde94ae1fe572408ba4e
BERNARD Damien <damien.bernard@epitech.eu>
2020-10-12 21:33:35 +0200

    add(tests): Adding more tests on ParseUInt/ParseInt/parseUFloat/parseFloat .

--- tests/ParserSpec.hs
+++ tests/ParserSpec.hs
@@ -25,3 +25,57 @@ spec = describe "Lib Parser Testing" $ do
             runParser (parseChar 'b') "bcda" `shouldBe` Right ('b', "cda")
         it "parse 'b' in \"abcd\"" $ do
             runParser (parseChar 'b') "abcd" `shouldSatisfy` isLeft
+    describe "Parse Unsigned Int" $ do
+        it "parse \"42aaa\" -> Right (42, \"aaa\")" $ do
+            runParser parseUInt "42aaa" `shouldBe` Right (42, "aaa")
+        it "parse \"aaa\" -> Left _" $ do
+            runParser parseUInt "aaa" `shouldSatisfy` isLeft
+            -- TODO: More Tests
+    describe "Parse Int" $ do
+        it "parse \"42aaa\" -> Right (42, \"aaa\")" $ do
+            runParser parseInt "42aaa" `shouldBe` Right (42, "aaa")
+        it "parse \"aaa\" -> Left _" $ do
+            runParser parseUInt "aaa" `shouldSatisfy` isLeft
+        it "parse \"-42aaa\" -> Right (-42, \"aaa\")" $ do
+            runParser parseInt "-42aaa" `shouldBe` Right (-42, "aaa")
+        it "parse \"aaa\" -> Left _" $ do
+            runParser parseUInt "aaa" `shouldSatisfy` isLeft
+            -- TODO: More Tests
+    describe "Parse Unsigned Float" $ do
+        it "parse \"42.0aaa\" -> Right (42.0, \"aaa\")" $ do
+            runParser parseFloat "42.0aaa" `shouldBe` Right (42.0, "aaa")
+        it "parse \"42.01aaa\" -> Right (42.01, \"aaa\")" $ do
+            runParser parseFloat "42.01aaa" `shouldBe` Right (42.01, "aaa")
+        it "parse \"a42aaa\" -> Left _" $ do
+            runParser parseFloat "a42aaa" `shouldSatisfy` isLeft
+            -- TODO: More Tests
+    describe "Parse Float" $ do
+        it "parse \"42.0aaa\" -> Right (42.0, \"aaa\")" $ do
+            runParser parseFloat "42.0aaa" `shouldBe` Right (42.0, "aaa")
+        it "parse \"-42.0aaa\" -> Right (-42, \"aaa\")" $ do
+            runParser parseFloat "-42.0aaa" `shouldBe` Right (-42.0, "aaa")
+        it "parse \"42.01aaa\" -> Right (42.01, \"aaa\")" $ do
+            runParser parseFloat "42.01aaa" `shouldBe` Right (42.01, "aaa")
+        it "parse \"-42.01aaa\" -> Right (-421, \"aaa\")" $ do
+            runParser parseFloat "-42.01aaa" `shouldBe` Right (-42.01, "aaa")
+        it "parse \"aaa\" -> Left _" $ do
+            runParser parseFloat "aaa" `shouldSatisfy` isLeft
+        it "parse \"a42aaa\" -> Left _" $ do
+            runParser parseFloat "a42aaa" `shouldSatisfy` isLeft
+--            -- TODO: More Tests
+
+testParseChar::Spec
+testParseChar =
+    describe "Parse Char" $ do
+        it "parse 'a' in \"abcd\" -> Right ('a', \"bcd\")" $ do
+            runParser (parseChar 'a') "abcd" `shouldBe` Right ('a', "bcd")
+        it "parse 'z' in \"abcd\" -> Left _" $ do
+            runParser (parseChar 'z') "abcd" `shouldSatisfy` isLeft
+        it "parse 'a' in \"aaaa\" -> Right ('a', \"aaa\")" $ do
+            runParser (parseChar 'a') "aaaa" `shouldBe` Right ('a', "aaa")
+        it "parse 'b' in \"baaa\" -> Right ('b', \"aaa\")" $ do
+            runParser (parseChar 'b') "baaa" `shouldBe` Right ('b', "aaa")
+        it "parse 'b' in \"bcda\" -> Right ('b', \"cda\")" $ do
+            runParser (parseChar 'b') "bcda" `shouldBe` Right ('b', "cda")
+        it "parse 'b' in \"abcd\" -> Left _" $ do
+            runParser (parseChar 'b') "abcd" `shouldSatisfy` isLeft
