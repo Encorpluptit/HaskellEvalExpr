288759fb8b18579adb48b95efc4af5bf441269f6
BERNARD Damien <damien.bernard@epitech.eu>
2020-10-15 09:31:52 +0200

    add(RecursiveParserDescent): Adding fcts for better error messages when parsing failed.

--- lib/RecursiveDescentRebirth.hs
+++ lib/RecursiveDescentRebirth.hs
@@ -23,52 +23,80 @@ import Boostrap (
 -- http://www.math.wichita.edu/~hammond/class-notes/section-counting-basics.html
 -- https://courses.lumenlearning.com/ivytech-collegealgebra/chapter/using-the-addition-and-multiplication-principles/
 
-data Expr = Num Float
+data Expr = Number Float
             | Add Expr Expr
             | Sub Expr Expr
             | Mul Expr Expr
             | Div Expr Expr
             | Power Expr Expr
             | Fail
-            deriving (Show, Eq)
+            deriving (Show, Eq, Ord)
+
+--class Multiply a b c | a b -> c where
+--class Multiply a b c | a b -> c where
+--  	(.*.) :: a -> b -> c
+--instance Multiply Expr Expr Expr where
+--    (.*.) = (*)
 
 parseAdd::Parser Expr
 parseAdd = parseNum
 
 parseNum::Parser Expr
-parseNum = Num <$> parseFloat
+parseNum = Number <$> parseFloat
 
-eval :: Expr -> Float
-eval e = case e of
-    Fail      -> 0
-    Add a b   -> eval a + eval b
-    Sub a b   -> eval a - eval b
-    Mul a b   -> eval a * eval b
-    Div a b   -> eval a / eval b
---  Div a b   -> eval a `div` eval b
-    Num n     -> n
+--eval :: Expr -> Float
+--eval e = case e of
+--    Fail      -> 0
+--    Add a b   -> eval a + eval b
+--    Sub a b   -> eval a - eval b
+--    Mul a b   -> eval a * eval b
+--    Div a b   -> eval a / eval b
+--    Num n     -> n
+
 --eval :: Expr -> Either String Float
 --eval e = case e of
---  Fail      -> Left "LOOOOOOOL"
---  Add a b   -> eval a + eval b
---  Sub a b   -> eval a - eval b
---  Mul a b   -> eval a * eval b
---  Div a b   -> eval a / eval b
-----  Div a b   -> eval a `div` eval b
---  Num n     -> Right n
+--    Fail      -> Left "LOOOOOOOL"
+--    Add a b   -> eval a + eval b
+--    Sub a b   -> eval a - eval b
+--    Mul a b   -> eval a * eval b
+--    Div a b   -> eval a / eval b
+--    Number n  -> Right n
+--    where
+
+eval :: Expr -> Either String Float
+eval e = case e of
+    Fail      -> Left "LOOOOOOOL"
+    Add a b   -> fct a b (+)
+    Sub a b   -> fct a b (-)
+    Mul a b   -> fct a b (*)
+    Div a b   -> fct a b (/)
+--    Div a b   -> eval a / eval b
+--    Add a b   -> eval a + eval b
+--    Sub a b   -> eval a - eval b
+--    Mul a b   -> eval a * eval b
+--    Div a b   -> eval a / eval b
+    Number n  -> Right n
+    where
+        fct a b op = case eval a of
+            Right x -> case eval b of
+                Right y     -> Right $ op x y
+                Left msg    -> Left msg
+            Left msg -> Left msg
 
 parseExpr::Parser Expr
 parseExpr = additive
     where
         additive    = applyOp Add '+' multitive <|> applyOp Sub '-' multitive <|> multitive
         multitive   = applyOp Mul '*' factor <|> applyOp Div '/' factor <|> factor
-        factor      = primary <|> Num <$> parseFloat
+        factor      = primary <|> Number <$> parseFloat
         primary     = parseChar '(' *> parseExpr <* parseChar ')'
         applyOp c o p = c <$> p <*> (parseChar o *> p)
 
 evalExpr :: String -> Either Error Float
 --evalExpr s = eval <$> runParser parseExpr s
-evalExpr s = eval <$> fct s
+evalExpr s = case eval <$> fct s of
+        Right a     -> a
+        Left msg    -> Left msg
     where
         fct s = case runParser parseExpr s of
             Right (a, [])    -> Right a
