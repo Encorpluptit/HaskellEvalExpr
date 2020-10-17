4031bfa8e69f937657544d7ffb2eb939098fbd9f
BERNARD Damien <damien.bernard@epitech.eu>
2020-10-17 13:44:43 +0200

    update(project): Updating Main for compilation and functional binary.

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ import System.Environment (getArgs)
 import System.Exit
 import System.IO
 import Text.Printf
-import RecursiveDescentRebirth
+import MonadicParser
 
 write_error :: String -> IO ()
 write_error str = hPutStrLn stderr ("Error : " ++ str)
@@ -15,11 +15,21 @@ exit_error str = do
     write_error str
     exitWith  $ ExitFailure 84
 
+--main :: IO()
+--main = do
+--    args <- getArgs
+--    case args of
+--        [] ->  exit_error "No args given"
+--        (x:_) -> case RecursiveDescentRebirth.evalExpr x of
+--            Right a     -> printf "%.2f\n" a
+--            Left msg    -> exit_error msg
 main :: IO()
 main = do
     args <- getArgs
     case args of
         [] ->  exit_error "No args given"
-        (x:_) -> case RecursiveDescentRebirth.evalExpr x of
-            Right a     -> printf "%.2f\n" a
-            Left msg    -> exit_error msg
+        (x:_) -> case evalExpr x of
+--            Just (a, []) -> printf "%.2f\n" a
+            Just (a, []) -> printf "%d\n" a
+            Just (a, xs) -> exit_error "Parsing Failed"
+            Nothing      -> exit_error "Parsing Failed"
