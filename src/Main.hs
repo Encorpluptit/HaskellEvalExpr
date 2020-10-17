b2c0b91815a0a63fc508c1e445195f331bad229e
BERNARD Damien <damien.bernard@epitech.eu>
2020-10-17 14:17:36 +0200

    update(project): Renaming Some utility fct.

--- src/Main.hs
+++ src/Main.hs
@@ -6,20 +6,20 @@ import System.IO
 import Text.Printf
 import MonadicParser
 
-write_error :: String -> IO ()
-write_error str = hPutStrLn stderr ("Error : " ++ str)
+writeError :: String -> IO ()
+writeError str = hPutStrLn stderr ("Error : " ++ str)
 
-exit_error :: String -> IO a
-exit_error [] = exitWith $ ExitFailure 84
-exit_error str = do
-    write_error str
+exitError :: String -> IO a
+exitError [] = exitWith $ ExitFailure 84
+exitError str = do
+    writeError str
     exitWith  $ ExitFailure 84
 
 main :: IO()
 main = do
     args <- getArgs
     case args of
-        [] ->  exit_error "No args given"
+        [] ->  exitError "No args given"
         (x:_) -> case evalExpr x of
             Just a      -> printf "%.2f\n" a
-            Nothing     -> exit_error "Parsing Failed"
+            Nothing     -> exitError "Parsing Failed"
